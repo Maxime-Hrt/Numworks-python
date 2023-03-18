22e49d59de7b795af8394ec1436d2848641cd8cf
Maxime <m.77.heurtevent@outlook.com>
2023-03-18 20:43:29 +0100

    🎉 Lauch new project

---
+++ odeSolver.py
@@ -0,0 +1,23 @@
+import math
+
+
+def f_euler(x, y):
+    return -y+math.exp(-x)
+
+
+def euler_resolution(h, y, x):
+    return y+h*f_euler(x, y)
+
+
+def eu(h, nb_points, y_init):
+    tab_coord = []
+    y = y_init
+    tab_coord.append(f"(0, {y_init})")
+
+    for i in range(1, nb_points):
+        tab_coord.append(f"({i*h}, {euler_resolution(h, y, i*h)})")
+        y = euler_resolution(h, y, i*h)
+    return tab_coord
+
+
+print(eu(.01, 1000, 1))
---
+++ openMethods.py
@@ -0,0 +1,62 @@
+# Replace the f and df return by the function
+
+def f(x):
+    return -9.6*pow(x, 5)-12*pow(x, 3)+10  # -1.6*pow(x, 6)-3*pow(x, 4)+10*x
+
+
+def df(x):
+    return -48*pow(x, 4)-36*pow(x, 2)
+
+
+def d2f(x):
+    return -192*pow(x, 3)-72*x
+
+
+def nr(x0, eps, p=1.0):
+    tab_v = []
+    tab_i = []
+    x = x0
+    i = 0
+    while abs(f(x)) > eps:
+        x = x - p*(f(x)/df(x))
+        i += 1
+        tab_v.append(x)
+        tab_i.append(i)
+    return tab_v, tab_i
+
+
+def hly(x0, eps=0.000000001):
+    tab_v = []
+    tab_i = []
+    x = x0
+    i = 0
+    while abs(f(x)) > eps:
+        x = x - (2*f(x)*df(x))/(2*pow(df(x), 2)-f(x)*d2f(x))
+        i += 1
+        tab_v.append(x)
+        tab_i.append(i)
+    return tab_v, tab_i
+
+
+def sct(min, max, eps=0.0001, p=1):
+    tab_v = []
+    tab_i = []
+    for i in range(50):
+        pente = (f(max)-f(min))/(max-min)
+        max_sup = max - p*(f(max)/pente)
+        if abs(max_sup - max) < eps:
+            tab_i.append(i)
+            tab_v.append(max_sup)
+            return tab_v, tab_i
+
+        tab_i.append(i)
+        tab_v.append(max_sup)
+        min = max
+        max = max_sup
+
+    return None
+
+
+print(nr(2, 0.000000001))
+print(sct(0, 4, 0.000000001))
+print(hly(1))
